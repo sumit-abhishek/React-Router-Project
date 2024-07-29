b97012f7e65b2c952eddc0cab47ec3a519d34602
sumit-abhishek <sumitabhi1997@gmail.com>
2024-07-29 10:51:05 +0530

    Learn react Router using this small project

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+import "./App.css";
+import Header from "./components/header/Header";
+import Home from "./components/home/Home";
+function App() {
+  return;
+}
+
+export default App;
---
+++ src/main.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  RouterProvider,
+  Route,
+} from "react-router-dom";
+import Layout from "./Layout.jsx";
+import Home from "./components/home/Home.jsx";
+import About from "./components/about/About.jsx";
+import User from "./components/user/User.jsx";
+import Contact from "./components/contact/Contact.jsx";
+import Github, { githubInfoLoader } from "./components/github/Github.jsx";
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<Layout />}>
+      <Route path="" element={<Home />} />
+      <Route path="about" element={<About />} />
+      <Route path="contact" element={<Contact />} />
+      <Route path="user/:userid" element={<User />} />
+      <Route loader={githubInfoLoader} path="github" element={<Github />} />
+    </Route>
+  )
+);
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
